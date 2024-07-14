4d119791516dd2a44924f251b3f62037343e2be0
uditha <udithaimesh4@gmail.com>
2024-07-14 16:15:59 +0530

    add date filter function

--- src/main/java/org/example/hakmana/view/scene/OverviewController.java
+++ src/main/java/org/example/hakmana/view/scene/OverviewController.java
@@ -7,10 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
@@ -24,6 +21,8 @@ import org.example.hakmana.view.component.AddDevButtonController;
 
 import javax.management.monitor.Monitor;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -33,27 +32,31 @@ public class OverviewController implements Initializable {
     @FXML
     TableView<LogEntry> historyTable;
     @FXML
-    TableColumn<LogEntry,String> dateColumn;
+    TableColumn<LogEntry, String> dateColumn;
     @FXML
-    TableColumn<LogEntry,String> processColumn;
+    TableColumn<LogEntry, String> processColumn;
     @FXML
-    TableColumn<LogEntry,String> detailsColumn;
+    TableColumn<LogEntry, String> detailsColumn;
     @FXML
-    private ChoiceBox <String> users;
+    private ChoiceBox<String> users;
     @FXML
-    private  ChoiceBox<String> devIds;
+    private ChoiceBox<String> devIds;
     @FXML
     private ChoiceBox<String> subChoiceBox;
     @FXML
     private Label label1;
-    private static OverviewController instance=null;
-    private ArrayList<String> allUsers=new ArrayList<>();
-    private OverviewTableData controller=new OverviewTableData();
-    private OverviewController(){}
+    @FXML
+    private DatePicker datePicker;
+    private static OverviewController instance = null;
+    private ArrayList<String> allUsers = new ArrayList<>();
+    private OverviewTableData controller = new OverviewTableData();
+
+    private OverviewController() {
+    }
 
     public static OverviewController getInstance() {
-        if(instance== null){
-            instance=new OverviewController();
+        if (instance == null) {
+            instance = new OverviewController();
             return instance;
         }
 
@@ -61,35 +64,28 @@ public class OverviewController implements Initializable {
         return instance;
     }
 
-    public void setValueForSubChoiceBox(ChoiceBox<String> subChoiceBox,String mainSelection){
+    public void setValueForSubChoiceBox(ChoiceBox<String> subChoiceBox, String mainSelection) {
         ObservableList<String> subOptions;
         label1.setVisible(true);
         if (mainSelection.equals("Desktop")) {
             subOptions = FXCollections.observableArrayList(Desktop.getDesktopInstance().getAllDesktops());
-        }
-        else if (mainSelection.equals("Laptops")) {
+        } else if (mainSelection.equals("Laptops")) {
             subOptions = FXCollections.observableArrayList(Laptops.getLaptopsInstance().getAllLaptops());
 
-        }
-        else if (mainSelection.equals("Photocopy Machines")) {
+        } else if (mainSelection.equals("Photocopy Machines")) {
             subOptions = FXCollections.observableArrayList(PhotocpyMchine.getPhotocpyMchineInstance().getAllPhotoCopyMachine());
-        }
-        else if (mainSelection.equals("Monitors")) {
+        } else if (mainSelection.equals("Monitors")) {
             subOptions = FXCollections.observableArrayList(Monitors.getMonitorInstance().getAllMonitors());
 
-        }
-        else if (mainSelection.equals("UPS")) {
+        } else if (mainSelection.equals("UPS")) {
             subOptions = FXCollections.observableArrayList(UPS.getUpsInstance().getAllUps());
 
-        }
-        else if (mainSelection.equals("Projectors")) {
+        } else if (mainSelection.equals("Projectors")) {
             subOptions = FXCollections.observableArrayList(Projectors.getProjectorsInstance().getAllProjectors());
 
-        }
-        else if (mainSelection.equals("Printers")) {
+        } else if (mainSelection.equals("Printers")) {
             subOptions = FXCollections.observableArrayList(Printer.getPrinterInstance().getAllPrinters());
-        }
-        else{
+        } else {
             subOptions = FXCollections.observableArrayList("all");
         }
         subChoiceBox.setItems(subOptions);
@@ -102,20 +98,18 @@ public class OverviewController implements Initializable {
     }
 
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //get SystemUsers
         label1.setVisible(false);
-        SystemUser devInstance=new SystemUser();
-        allUsers=devInstance.getAllSystemUsers();
-        if(allUsers!=null){
+        SystemUser devInstance = new SystemUser();
+        allUsers = devInstance.getAllSystemUsers();
+        if (allUsers != null) {
             users.getItems().addAll(allUsers);
             users.setValue("all");
         }
         //set devIds
-        ObservableList<String> mainOptions = FXCollections.observableArrayList("Desktop","Laptops","Photocopy Machines","Monitors","UPS","Projectors","Printers","all");
+        ObservableList<String> mainOptions = FXCollections.observableArrayList("Desktop", "Laptops", "Photocopy Machines", "Monitors", "UPS", "Projectors", "Printers", "all");
         devIds.setItems(mainOptions);
         devIds.setValue("all");
 
@@ -129,11 +123,12 @@ public class OverviewController implements Initializable {
         devIds.setOnAction(this::filter);
         users.setOnAction(this::filter);
         subChoiceBox.setOnAction(this::filter);
+        datePicker.setOnAction(this::filter);
         //add data to the table
 
-        ObservableList<LogEntry> list= controller.setColumnData(users,devIds,subChoiceBox);
-        dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry,String>("timestamp"));
-        processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry,String>("process"));
+        ObservableList<LogEntry> list = controller.setColumnData(users, devIds, subChoiceBox);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("timestamp"));
+        processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("process"));
         detailsColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("details"));
         historyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
         historyTable.setItems(list);
@@ -141,15 +136,40 @@ public class OverviewController implements Initializable {
     }
 
     private void filter(ActionEvent actionEvent) {
+        ObservableList<LogEntry> list = controller.setColumnData(users, devIds, subChoiceBox);
+        System.out.println(list.isEmpty());
+        LocalDate date = datePicker.getValue();
+        DateTimeFormatter newFormat = DateTimeFormatter.ISO_LOCAL_DATE;
+        ObservableList<LogEntry> filteredList = FXCollections.observableArrayList();
+        if (date!=null&& !list.isEmpty()) {
+            for (LogEntry l : list) {
+                String dateTimeString = l.getTimestamp();
+                String dateStringArray[] = dateTimeString.split(" ", 2);
+                LocalDate localDate = LocalDate.parse(dateStringArray[0], newFormat);
+                if (localDate.isEqual(date) || datePicker.getValue() == null) {
+                    filteredList.add(l);
+                    dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("timestamp"));
+                    processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("process"));
+                    detailsColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("details"));
+                    historyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+                    historyTable.setItems(filteredList);
+                } else {
+                    dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("timestamp"));
+                    processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("process"));
+                    detailsColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("details"));
+                    historyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+                    historyTable.setItems(filteredList);
+                }
+            }
 
-        ObservableList<LogEntry> list= controller.setColumnData(users,devIds,subChoiceBox);
-        dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry,String>("timestamp"));
-        processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry,String>("process"));
-        detailsColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("details"));
-        historyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
-        historyTable.setItems(list);
+        }
+        else{
+            dateColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("timestamp"));
+            processColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("process"));
+            detailsColumn.setCellValueFactory(new PropertyValueFactory<LogEntry, String>("details"));
+            historyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+            historyTable.setItems(list);
+        }
+
     }
-
-
-
 }
