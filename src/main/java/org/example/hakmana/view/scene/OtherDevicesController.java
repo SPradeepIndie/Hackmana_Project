9ac3155a57a35b2c6ebc3a95beeff1081dd785b3
Sanjaya Pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-07-19 11:23:31 +0530

    Fix the data repeating issue in the Other device table

--- src/main/java/org/example/hakmana/view/scene/OtherDevicesController.java
+++ src/main/java/org/example/hakmana/view/scene/OtherDevicesController.java
@@ -72,12 +72,7 @@ public class OtherDevicesController implements Initializable {
         inactiveClmn.setCellValueFactory(new PropertyValueFactory<OtherDevices, Integer>("numInactiveDev"));
         repairClmn.setCellValueFactory(new PropertyValueFactory<OtherDevices, Integer>("numRepairingDev"));
         totalClmn.setCellValueFactory(new PropertyValueFactory<OtherDevices, Integer>("totalDev"));
-        update();
-    }
 
-    public void update(){
-        otherDevicesDb.setTblRowLoaded(false);
-        OtherDevices.setDevicesLoaded(false);
         addTblRow();
         addTableSelectionListener();
     }
@@ -92,12 +87,6 @@ public class OtherDevicesController implements Initializable {
     public void setDashboardPathFinderControllerD(PathFinderController dashboardPathFinderControllerD) {
         this.dashboardPathFinderControllerD = dashboardPathFinderControllerD;
     }
-    public static ScrollPane getDashboardBodyScrollpaneD() {
-        return dashboardBodyScrollpaneD;
-    }
-    public static void setDashboardBodyScrollpaneD(ScrollPane dashboardBodyScrollpaneD) {
-        OtherDevicesController.dashboardBodyScrollpaneD = dashboardBodyScrollpaneD;
-    }
 
     public String getDevName() {
         return devName;
@@ -109,7 +98,7 @@ public class OtherDevicesController implements Initializable {
             public void changed(ObservableValue<? extends OtherDevices> observable, OtherDevices oldValue, OtherDevices newValue) {
                 if (newValue != null) {
                     devName = newValue.getDev();
-                    ViewMore.setDisable(true);
+                    ViewMore.setDisable(false);
                 }
             }
         });
@@ -139,7 +128,14 @@ public class OtherDevicesController implements Initializable {
         if (clickedButton.isPresent() && clickedButton.get() == ButtonType.CANCEL) {
             dialog.close();
         }
-
-        update();
+        tableViewRefresh();
     }
+
+    private void tableViewRefresh(){
+        otherDevicesDb.setTblRowLoaded(false);
+        OtherDevices.setDevicesLoaded(false);
+        otherDevicesDb.getObservableOtherDevices();
+        otherDeviceTblView.refresh();
+    }
+
 }
